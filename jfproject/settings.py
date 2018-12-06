4df0dbf728d0052cbf05be6e63cd51cc0f602bc7
Jeric Bandong <jericfederer97@gmail.com>
2018-12-06 23:49:53 +0900

    Installed Postgresql and created new database

--- jfproject/settings.py
+++ jfproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'jfproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jfproject',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
