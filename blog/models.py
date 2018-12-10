587f1a6dbcfba22e2853728be778e4ddec960494
Jeric Bandong <jericfederer97@gmail.com>
2018-12-11 08:21:16 +0900

    Blog prototype done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    # blog titles on admin page
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:50]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
